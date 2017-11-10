699e70659b85fb517ea1199ab1c41f84c346d29e
UnknownEvil <reapor12@hotmail.com>
2017-11-09 19:27:13 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -25,7 +25,7 @@ function render() {
         ctx.beginPath();
         ctx.moveTo(landPoints[i][0].x, landPoints[i][0].y);
         
-        for (let j = 0; j < landPoints[i].length - 2; j += 2) {
+        for (let j = 0; j < landPoints[i].length - 2; j += 3) {
             ctx.quadraticCurveTo(landPoints[i][j + 1].x, landPoints[i][j + 1].y, landPoints[i][j + 2].x, landPoints[i][j + 2].y);
         }
         
