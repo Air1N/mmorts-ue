36407a9c27549fc0d8906e07c2a4bc275bba0557
UnknownEvil <reapor12@hotmail.com>
2017-11-12 17:35:49 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,27 +16,6 @@ function render() {
         ctx.lineJoin = "round";
         ctx.lineCap = "round";
         
-        ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.moveTo(landPoints[i][0].x + 1, landPoints[i][0].y + 1);
-
-        for (let j = 1; j < landPoints[i].length - 3; j += 2) {
-            ctx.bezierCurveTo(landPoints[i][j].x + 1, landPoints[i][j].y + 1, landPoints[i][j + 1].x + 1, landPoints[i][j + 1].y + 1, landPoints[i][j + 2].x + 1, landPoints[i][j + 2].y + 1, landPoints[i][j + 3].x + 1, landPoints[i][j + 3].y + 1);
-        }
-
-        ctx.closePath();
-        ctx.fill();
-    
-        ctx.beginPath();
-        ctx.moveTo(landPoints[i][0].x - 1, landPoints[i][0].y - 1);
-        
-        for (let j = 1; j < landPoints[i].length - 3; j += 2) {
-            ctx.bezierCurveTo(landPoints[i][j].x - 1, landPoints[i][j].y - 1, landPoints[i][j + 1].x - 1, landPoints[i][j + 1].y - 1, landPoints[i][j + 2].x - 1, landPoints[i][j + 2].y - 1, landPoints[i][j + 3].x - 1, landPoints[i][j + 3].y - 1);
-        }
-
-        ctx.closePath();
-        ctx.fill();
-        
         ctx.fillStyle = "green";
         ctx.beginPath();
         ctx.moveTo(landPoints[i][0].x, landPoints[i][0].y);
@@ -47,6 +26,20 @@ function render() {
         
         
         ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+        
+        ctx.fillStyle = "green";
+        ctx.beginPath();
+        ctx.moveTo(landPoints[i][0].x, landPoints[i][0].y);
+        
+        for (let j = 1; j < landPoints[i].length - 3; j += 3) {
+            ctx.bezierCurveTo(landPoints[i][j].x, landPoints[i][j].y, landPoints[i][j + 1].x, landPoints[i][j + 1].y, landPoints[i][j + 2].x, landPoints[i][j + 2].y, landPoints[i][j + 3].x, landPoints[i][j + 3].y);
+        }
+        
+        
+        ctx.closePath();
+        ctx.stroke();
         ctx.fill();
     }
 
