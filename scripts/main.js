d3f080e7a7eb243d0342cc821bf24dcef0c9520c
UnknownEvil <reapor12@hotmail.com>
2017-11-12 17:31:24 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ function render() {
         ctx.beginPath();
         ctx.moveTo(landPoints[i][0].x + 1, landPoints[i][0].y + 1);
 
-        for (let j = 1; j < landPoints[i].length - 3; j += 4) {
+        for (let j = 1; j < landPoints[i].length - 3; j += 6) {
             ctx.bezierCurveTo(landPoints[i][j].x + 1, landPoints[i][j].y + 1, landPoints[i][j + 1].x + 1, landPoints[i][j + 1].y + 1, landPoints[i][j + 2].x + 1, landPoints[i][j + 2].y + 1, landPoints[i][j + 3].x + 1, landPoints[i][j + 3].y + 1);
         }
 
@@ -30,7 +30,7 @@ function render() {
         ctx.beginPath();
         ctx.moveTo(landPoints[i][0].x - 1, landPoints[i][0].y - 1);
         
-        for (let j = 1; j < landPoints[i].length - 3; j += 4) {
+        for (let j = 1; j < landPoints[i].length - 3; j += 6) {
             ctx.bezierCurveTo(landPoints[i][j].x - 1, landPoints[i][j].y - 1, landPoints[i][j + 1].x - 1, landPoints[i][j + 1].y - 1, landPoints[i][j + 2].x - 1, landPoints[i][j + 2].y - 1, landPoints[i][j + 3].x - 1, landPoints[i][j + 3].y - 1);
         }
 
@@ -41,7 +41,7 @@ function render() {
         ctx.beginPath();
         ctx.moveTo(landPoints[i][0].x, landPoints[i][0].y);
         
-        for (let j = 1; j < landPoints[i].length - 3; j += 4) {
+        for (let j = 1; j < landPoints[i].length - 3; j += 6) {
             ctx.bezierCurveTo(landPoints[i][j].x, landPoints[i][j].y, landPoints[i][j + 1].x, landPoints[i][j + 1].y, landPoints[i][j + 2].x, landPoints[i][j + 2].y, landPoints[i][j + 3].x, landPoints[i][j + 3].y);
         }
         
