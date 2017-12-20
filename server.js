f51192f07c211c199df35383de4ce1e91d767703
UnknownEvil <reapor12@hotmail.com>
2017-12-20 08:39:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ let players = 200;
 
 for (let i = 0; i < continents; i++) {
     landPoints.push([{
-        x: Math.random() * 8000,
-        y: Math.random() * 8000
+        x: Math.random() * 60000,
+        y: Math.random() * 60000
     }]);
 }
 
