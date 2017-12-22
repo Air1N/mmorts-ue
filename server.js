6c1831af571b2b637078fe866936b1f2add1581c
UnknownEvil <reapor12@hotmail.com>
2017-12-22 15:01:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -84,7 +84,14 @@ function enhanceLand(k) {
     for (let i = 0; i < landPoints[k].length - 1; i += 11) {
         for (let j = 0; j < 10; j++) {
             landPoints[k].splice(i + 1 + j, 0, {x: (landPoints[k][i].x - landPoints[k][i + 1 + j].x) / j, y: (landPoints[k][i].y - landPoints[k][i + 1 + j].y) / j});
-            console.log(landPoints[k][i + 1 + j]);
+            
+            console.log("First");
+            console.log(landPoints[k][i].x);
+            console.log(landPoints[k][i].y);
+            
+            console.log("Second");
+            console.log(landPoints[k][i + 1 + j].x);
+            console.log(landPoints[k][i + 1 + j].y);
         }
     }
 }
