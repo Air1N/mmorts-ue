ca799b65672f97270df67b6d18c7e2ca5eae7b8f
UnknownEvil <reapor12@hotmail.com>
2017-12-22 15:44:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ function generateLand(k, f) {
 function enhanceLand(k) {
     for (let i = 0; i < landPoints[k].length - 1; i += 9) {
         for (let j = 0; j < 8; j++) {
-            landPoints[k].splice(i + 1, 0, {
+            landPoints[k].splice(i + 1 + j, 0, {
                 x: landPoints[k][i].x + ((landPoints[k][i + 1 + j].x - landPoints[k][i].x) / 10) * (j + 1), 
                 y: landPoints[k][i].y + ((landPoints[k][i + 1 + j].y - landPoints[k][i].y) / 10) * (j + 1)
             });
