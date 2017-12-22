06b2d0f637fac454a3343d5686b59a2bd163935e
UnknownEvil <reapor12@hotmail.com>
2017-12-22 14:10:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -64,9 +64,8 @@ function generateLand(k, f) {
     if (f >= landComplexity - landComplexity / 10) {
         biasx = Math.round((landPoints[k][0].x - currentx) / (landComplexity - f));
         biasy = Math.round((landPoints[k][0].y - currenty) / (landComplexity - f));
-        
-        console.log(biasx);
-        console.log(biasy);
+        console.log(landPoints[k][0].x - currentx);
+        console.log(landComplexity - f);
     } else {
         for (let i = 0; i < landPoints[k].length; i++) {
             if (landPoints[k][i].x == currentx + randomx && landPoints[k][i].y == currenty + randomy) {
