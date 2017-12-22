97d17bd59a2619d24a8a87792b9c68757c5f3a13
UnknownEvil <reapor12@hotmail.com>
2017-12-22 12:53:33 -0500

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -19,9 +19,9 @@ let property = [];
 
 let players = 200;
 
-let zoomLevel = 1 / 64;
+let zoomLevel = 1 / 256;
 
-const landSize = 0.0000005;
+const landSize = 0.00005;
 
 window.onmousedown = function(e) {
 	mouseDown = true;
