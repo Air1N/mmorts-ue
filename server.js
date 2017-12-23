4b1b206d3b235916df539ec0e7ad4a82fd7311bb
UnknownEvil <reapor12@hotmail.com>
2017-12-22 20:13:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -81,14 +81,12 @@ function generateLand(k, f) {
 }
 
 function enhanceLand(k) {
-    for (let j = 1; j < Math.pow(2, 5); j *= 2) {
+    for (let j = 1; j < 10, j++) {
     for (let i = 0; i < landPoints[k].length - 1; i += 1 + j) {
-        for (let v = 0; v < j; v++) {
-            landPoints[k].splice(i + 1 + v, 0, {
-                x: landPoints[k][i].x + ((landPoints[k][i + 1 + v].x - landPoints[k][i].x) / (j + 1)) * (v + 1) + (Math.random() * 2 - 1) * landSize / j / 4, 
-                y: landPoints[k][i].y + ((landPoints[k][i + 1 + v].y - landPoints[k][i].y) / (j + 1)) * (v + 1) + (Math.random() * 2 - 1) * landSize / j / 4
+            landPoints[k].splice(i + 1, 0, {
+                x: landPoints[k][i].x + ((landPoints[k][i + 1].x - landPoints[k][i].x) / (j + 1)) + (Math.random() * 2 - 1) * landSize / j / 4, 
+                y: landPoints[k][i].y + ((landPoints[k][i + 1].y - landPoints[k][i].y) / (j + 1)) + (Math.random() * 2 - 1) * landSize / j / 4
             });
-        }
     }
     }
 }
