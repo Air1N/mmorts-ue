9324e940c76ff41b0e83be48e2ad88706a083c21
UnknownEvil <reapor12@hotmail.com>
2017-12-24 04:12:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ http.listen(port, function() {
 });
 
 function generateLand(k, f) {
-    let randomx = Math.round(landSize * Math.round(Math.random() * 2 - 1) * 10) / 10;
-    let randomy = Math.round(landSize * Math.round(Math.random() * 2 - 1) * 10) / 10;
+    let randomx = Math.round(landSize * Math.round(Math.random() * 2 - 1) * 1000) / 1000;
+    let randomy = Math.round(landSize * Math.round(Math.random() * 2 - 1) * 1000) / 1000;
 
     let currentx = landPoints[k][landPoints[k].length - 1].x;
     let currenty = landPoints[k][landPoints[k].length - 1].y;
@@ -112,8 +112,8 @@ function enhanceLand(k) {
     if (count > landPoints[k].length - 2) count = 0;
     
     landPoints[k].splice(count + 1, 0, {
-        x: Math.round((landPoints[k][count].x + ((landPoints[k][count + 1].x - landPoints[k][count].x) / 2) + Math.round((Math.random() * 2 - 1)) * landSize / Math.pow(2, (landVersion + 1) / 1.3)) * 10) / 10,
-        y: Math.round((landPoints[k][count].y + ((landPoints[k][count + 1].y - landPoints[k][count].y) / 2) + Math.round((Math.random() * 2 - 1)) * landSize / Math.pow(2, (landVersion + 1) / 1.3)) * 10) / 10
+        x: Math.round((landPoints[k][count].x + ((landPoints[k][count + 1].x - landPoints[k][count].x) / 2) + Math.round((Math.random() * 2 - 1)) * landSize / Math.pow(2, (landVersion + 1) / 1.3)) * 1000) / 1000,
+        y: Math.round((landPoints[k][count].y + ((landPoints[k][count + 1].y - landPoints[k][count].y) / 2) + Math.round((Math.random() * 2 - 1)) * landSize / Math.pow(2, (landVersion + 1) / 1.3)) * 1000) / 1000
     });
 
     landVersion++;
@@ -167,7 +167,7 @@ for (let i = 0; i < continents; i++) {
     }]);
 }
 
-const landSize = 1000;
+const landSize = 50;
 
 for (let mm = 0; mm < continents; mm++) {
     console.log('generating continent ' + (mm + 1));
