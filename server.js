964f182fcf895789b0b81cb9d82690f6e5df4448
UnknownEvil <reapor12@hotmail.com>
2017-12-24 04:34:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ let idList = [];
 let continents = 1;
 let players = 200;
 
-let initialSize = 3;
+let initialSize = 5;
 
 io.on('connection', function(socket) {
     userID = socket.handshake.address;
