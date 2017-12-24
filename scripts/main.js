86e7c363422d68b959f9f55fa3cca4e15a4c8494
UnknownEvil <reapor12@hotmail.com>
2017-12-23 23:10:32 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -24,15 +24,13 @@ function render() {
                 continue;
             }
             
-            if (landPoints[i][j].y < 0 - 150 * zoomLevel || landPoints[i][j].y > display.height + 150 * zoomLevel) {
+            if (landPoints[i][j].y < 0 - 250 * zoomLevel || landPoints[i][j].y > display.height + 250 * zoomLevel) {
                 continue;
             }
             
             var xc = (landPoints[i][j].x + landPoints[i][j + Math.max(Math.round(zoomLayers / zoomLevel), 1)].x) / 2;
             var yc = (landPoints[i][j].y + landPoints[i][j + Math.max(Math.round(zoomLayers / zoomLevel), 1)].y) / 2;
             ctx.quadraticCurveTo(landPoints[i][j].x, landPoints[i][j].y, xc, yc);
-            ctx.fillStyle = "red";
-            ctx.fillRect(landPoints[i][j].x - 10, landPoints[i][j].y - 10, 20, 20);
         }
 
         ctx.quadraticCurveTo(landPoints[i][j].x, landPoints[i][j].y, landPoints[i][landPoints[i].length - 1].x, landPoints[i][landPoints[i].length - 1].y);
